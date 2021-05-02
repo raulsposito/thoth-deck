710f54413965edce76c8fa7c1ea64882c31fb663
raulsposito <raulsposito@gmail.com>
2021-05-02 11:36:44 -0300

    add all major arcana to data file

--- src/Components/data.js
+++ src/Components/data.js
@@ -5,6 +5,90 @@ const data = [
         title: 'The Fool',
         cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/05/the-fool-thoth-tarot.png'
     },
+    {
+        title: 'The Magus',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/05/the-magus.png'
+    },
+    {
+        title: 'The Priestess',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/05/the-pristess-thoh-tarot.png'
+    },
+    {
+        title: 'The Empress',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/05/the-empress-thoth-tarot.png'
+    },
+    {
+        title: 'The Emperor',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/05/the-emperor-thoth-tarot.png'
+    },
+    {
+        title: 'The Hierophant',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/05/the-hierophant-thoth.png'
+    },
+    {
+        title: 'The Lovers',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/05/the-lovers-thoth.png'
+    },
+    {
+        title: 'The Chariot',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/05/the-chariot-thoth.png'
+    },
+    {
+        title: 'The Adjustment',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/05/the-adjustment-thoth.png'
+    },
+    {
+        title: 'The Hermit',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/05/hermit-thoth.jpg'
+    },
+    {
+        title: 'The Fortune',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/the-fortune-thoth.png'
+    },
+    {
+        title: 'The Lust',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/the-lust-thoth.png'
+    },
+    {
+        title: 'The Hanged Man',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/the-hanged-man-thoth.png'
+    },
+    {
+        title: 'The Death',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/thoth-tarot-death.png'
+    },
+    {
+        title: 'The Art',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/the-art-thoth.png'
+    },
+    {
+        title: 'The Devil',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/the-devil-thoth.png'
+    },
+    {
+        title: 'The Tower',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/the-tower-thoth.png'
+    },
+    {
+        title: 'The Start',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/the-star-thoth.png'
+    },
+    {
+        title: 'The Moon',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/the-moon-thoth.png'
+    },
+    {
+        title: 'The Sun',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/the-sun-thoth.png'
+    },
+    {
+        title: 'The Aeon',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/the-aeon-thoth.png'
+    },
+    {
+        title: 'The Universe',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/the-universe-thoth.png'
+    }
 ]
 
 export default data 
