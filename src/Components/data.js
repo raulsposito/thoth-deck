3a31f7cc0fedae1421330e2f2af7d3a44ee2b1bc
raulsposito <raulsposito@gmail.com>
2021-05-02 21:08:14 -0300

    add whole deck to data file

--- src/Components/data.js
+++ src/Components/data.js
@@ -88,7 +88,231 @@ const data = [
     {
         title: 'The Universe',
         cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/the-universe-thoth.png'
-    }
+    },
+    {
+        title: 'Ace of Wands',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/ace-of-wands-thoth.png'
+    },
+    {
+        title: '2 of Wands',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/2-of-wands-thoth.png'
+    },
+    {
+        title: '3 of Wands',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/3-of-wands-thoth.png'
+    },
+    {
+        title: '4 of Wands',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/4-of-wands-thoth.png'
+    },
+    {
+        title: '5 of Wands',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/5-of-wands-thoth.png'
+    },
+    {
+        title: '6 of Wands',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/6-of-wands-thoth.png'
+    },
+    {
+        title: '7 of Wands',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/6-of-wands-thoth.png'
+    },
+    {
+        title: '8 of Wands',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/8-of-wands-thoth.png'
+    },
+    {
+        title: '9 of Wands',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/9-of-wands-thoth.png'
+    },
+    {
+        title: '10 of Wands',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/10-of-wands-thoth.png'
+    },
+    {
+        title: 'Prince of Wands',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/prince-of-wands-thoth.png'
+    },
+    {
+        title: 'Princess of Wands',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/princess-of-wands-thoth.png'
+    },
+    {
+        title: 'Queen of Wands',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/queen-of-wands-thoth.png'
+    },
+    {
+        title: 'Knight of Wands',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/knight-of-wands-thoth.png'
+    },
+    {
+        title: 'Ace of Cups',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/ace-of-cups-thoth.png'
+    },
+    {
+        title: '2 of Cups',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/2-of-cups-thoth.png'
+    },
+    {
+        title: '3 of Cups',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/3-of-cups-thoth.png'
+    },
+    {
+        title: '4 of Cups',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/4-of-cups-thoth.png'
+    },
+    {
+        title: '5 of Cups',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/5-of-cups-thoth.png'
+    },
+    {
+        title: '6 of Cups',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/6-of-cups-thoth.png'
+    },
+    {
+        title: '7 of Cups',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/7-of-cups-thoth.png'
+    },
+    {
+        title: '8 of Cups',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/8-of-cups-thoth.png'
+    },
+    {
+        title: '9 of Cups',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/9-of-cups-thoth.png'
+    },
+    {
+        title: '10 of Cups',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/10-of-cups-thoth.png'
+    },
+    {
+        title: 'Prince of Cups',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/prince-of-cups-thoth.png'
+    },
+    {
+        title: 'Princess of Cups',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/princess-of-cups-thoth.png'
+    },
+    {
+        title: 'Queen of Cups',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/queen-of-cups-thoth.png'
+    },
+    {
+        title: 'Knight of Cups',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/knight-of-cups-thoth.png'
+    },
+    {
+        title: 'Ace of Swords',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/ace-of-swords-thoth.png'
+    },
+    {
+        title: '2 of Swords',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/two-of-swords-thoth.png'
+    },
+    {
+        title: '3 of Swords',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/3-of-swords-thoth.png'
+    },
+    {
+        title: '4 of Swords',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/4-of-swords-thoth.png'
+    },
+    {
+        title: '5 of Swords',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/5-of-swords-thoth.png'
+    },
+    {
+        title: '6 of Swords',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/6-of-swords-thoth.png'
+    },
+    {
+        title: '7 of Swords',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/7-of-swords-thoth.png'
+    },
+    {
+        title: '8 of Swords',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/8-of-swords-thoth.png'
+    },
+    {
+        title: '9 of Swords',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/9-of-swords-thoth.png'
+    },
+    {
+        title: '10 of Swords',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/10-of-swords-thoth.png'
+    },
+    {
+        title: 'Prince of Swords',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/prince-of-swords-thoth.png'
+    },
+    {
+        title: 'Princess of Swords',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/princess-of-swords-thoth.png'
+    },
+    {
+        title: 'Queen of Swords',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/queen-of-swords-thoth.png'
+    },
+    {
+        title: 'Knight of Swords',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/knight-of-swords-thoth.png'
+    },
+    {
+        title: 'Ace of Disks',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/ace-of-disks-thoth.png'
+    },
+    {
+        title: '2 of Disks',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/2-of-disk-thoth.png'
+    },
+    {
+        title: '3 of Disks',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/3-of-disks-thoth.png'
+    },
+    {
+        title: '4 of Disks',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/4-of-disks-thoth.png'
+    },
+    {
+        title: '5 of Disks',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/5-of-disks-thoth.png'
+    },
+    {
+        title: '6 of Disks',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/6-of-disk-thoth.png'
+    },
+    {
+        title: '7 of Disks',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/7-of-disks-thoth.png'
+    },
+    {
+        title: '8 of Disks',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/8-of-disks-thoth.png'
+    },
+    {
+        title: '9 of Disks',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/9-of-disks-thoth.png'
+    },
+    {
+        title: '10 of Disks',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/10-of-disks-thoth.png'
+    },
+    {
+        title: 'Prince of Disks',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/prince-of-disks.png'
+    },
+    {
+        title: 'Princess of Disks',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/princess-of-disks-thoth.png'
+    },
+    {
+        title: 'Queen of Disks',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/queen-of-disks-thoth.png'
+    },
+    {
+        title: 'Knight of Disks',
+        cover: 'https://cdn-0.tarotx.net/wp-content/uploads/2019/06/knight-of-disks-thoth.png'
+    },
 ]
 
 export default data 
